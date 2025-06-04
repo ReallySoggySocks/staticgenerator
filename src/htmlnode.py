ab20e04aebe23432feac7cbd4295a4c3309352bd
ReallySoggySocks <havenkain@gmail.com>
2025-06-04 14:43:48 -0500

    created test for htmlnode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -13,6 +13,12 @@ class HTMLNode():
         for item in self.props:
             prop += f' {item}="{self.props[item]}"'
         return prop
+    
+    def __eq__(self, other):
+        if self.tag == other.tag and self.value == other.value and self.children == other.children and self.props == other.props:
+            return True
+        else:
+            return False
 
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -3,7 +3,19 @@ import unittest
 from htmlnode import HTMLNode
 
 class TestHTMLNode(unittest.TestCase):
-    pass
+    def test_props_to_html(self):
+        node = HTMLNode(None, "this is a htmlnode", None, {"href" : "https://www.google.com", "target" : "_blank"})
+        self.assertEqual(node.props_to_html(), ' href="https://www.google.com" target="_blank"')
+
+    def test_none(self):
+        node = HTMLNode()
+        node2 = HTMLNode(None, None, None, None)
+        self.assertEqual(node, node2)
+
+    def test_repr(self):
+        node = HTMLNode(None, "this is a htmlnode", None, {"href" : "https://www.google.com", "target" : "_blank"})
+        self.assertEqual(node.__repr__(), "HTMLNode(None, this is a htmlnode, None, {'href': 'https://www.google.com', 'target': '_blank'})")
+    
 
 if __name__ == "__main__":
     unittest.main()
